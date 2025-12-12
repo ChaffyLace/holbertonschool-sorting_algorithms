807fba8b30a52c25c387ccc1f542569415306cda
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2025-12-12 10:26:00 +0100

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,25 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+#include <stdio.h>
+
+/* Print functions */
+void print_array(const int *array, size_t size);
+void print_list(const struct listint_s *list);
+
+/* Doubly linked list structure */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* Sorting algorithms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
